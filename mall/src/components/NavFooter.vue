17f08261954de05e53c23f535f26b79e276787be
propoet <propoet@163.com>
2020-02-25 02:39:42 +0800

    2.25

---
+++ mall/src/components/NavFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        footer
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ mall/src/components/NavHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        header
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
